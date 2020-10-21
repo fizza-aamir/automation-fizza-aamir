0d5ae3a1a5e5a5a91849847dd7415a1def9bc7ff
Fizza Aamir Faried <54666683+fizza-aamir@users.noreply.github.com>
2020-10-21 11:30:39 +0200

    created a .gitignore and created a test project

---
+++ frontend-tests-fizza/cypress/integration/regression-test-suite.spec.js
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+
+// this is a test suite
+describe('Regression test suite', function(){
+
+    //This is a test case
+    it('test case 1', function(){
+        cy.visit('http://localhost:3000')
+    })
+})
